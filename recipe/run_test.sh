257d6561a5d76f560a78bf4669bfa2968821de25
Silvio Traversaro <silvio@traversaro.it>
2021-08-31 10:14:29 +0200

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Check files
+test -f ${PREFIX}/include/backward.hpp
+test -f ${PREFIX}/lib64/backward/BackwardConfig.cmake
+
+# Test example build
 mkdir build
 cd build
 cmake $RECIPE_DIR -DCMAKE_BUILD_TYPE=Debug
