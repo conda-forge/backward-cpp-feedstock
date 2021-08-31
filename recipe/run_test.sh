cc17a48c389e41a5417522f1dba7c313b33da698
Silvio Traversaro <silvio@traversaro.it>
2021-08-31 11:21:01 +0200

    Bump build and add test to check names (#19)

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Check files
+test -f ${PREFIX}/include/backward.hpp
+test -f ${PREFIX}/lib/backward/BackwardConfig.cmake
+
+# Test example build
 mkdir build
 cd build
 cmake $RECIPE_DIR -DCMAKE_BUILD_TYPE=Debug
